cab2680378bdcf254b56233e10b454f4090cdc5d
Brittney Stanley <brittneyrstanley07@hotmail.com>
2022-03-17 19:41:34 -0400

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,39 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if "Arapahoe" in counties and "El Paso" not in counties:
+    print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties")
+
+for county in counties:
+    print(county)
+
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+
+for num in range(5):
+    print(num)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict.keys():
+    print(county)
+
+for voters in counties_dict.values():
+    print(voters)
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+for key, value in counties_dict.items():
+    print(county, voters)    
+
